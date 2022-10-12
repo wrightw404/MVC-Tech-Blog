4bc17c05ab7895d914ad6b1f47f98a79082e4991
Will <wrightw404@gmail.com>
2022-10-11 20:11:13 -0700

    updated user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -15,7 +15,7 @@ router.post('/', async (req, res) => {
     });
 
     req.session.save(() => {
-      req.session.userId = newUser.id;
+      req.session.user_id = newUser.id;
       req.session.username = newUser.username;
       req.session.loggedIn = true;
 
@@ -47,7 +47,7 @@ router.post('/login', async (req, res) => {
     }
 
     req.session.save(() => {
-      req.session.userId = user.id;
+      req.session.user_id = user.id;
       req.session.username = user.username;
       req.session.loggedIn = true;
 
